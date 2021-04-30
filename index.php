f2d65aedafe3617ecca9ea245714f713393da0c7
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-04-30 15:09:07 +0200

    ajout photo

--- index.php
+++ index.php
@@ -11,30 +11,31 @@
     <link rel="stylesheet" href="assets/css/bootstrap.css">
 </head>
 <body>
-    <main class="container">
-        <div class="row">
-            <div class="col-xl-6 col-sm-12">
-                <img src="assets/images/phone.png" alt="mobile">
+    <main class="container mt-5">
+        <div class="row d-flex align-items-center">
+            <div class="col-xl-2"></div>
+            <div class="col-xl-4 col-sm-12">
+                <img src="assets/images/phone.png"  alt="mobile">
             </div>
             <div class="col-xl-6 col-sm-12">
+                <img src="assets/images/logo.png" class="w-50 ml-5 mb-5" alt="logo">   
             <form>
-                <fieldset>  
-                              
-
+                <fieldset>
                     <div class="form-group">               
-                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" required>               
+                        <input type="email" class="form-control w-75" id="email" aria-describedby="emailHelp" placeholder="Email" required>               
                     </div>
                     <div class="form-group">             
-                        <input type="password" class="form-control" id="password" placeholder="Password" required>                    
+                        <input type="password" class="form-control w-75" id="password" placeholder="Password" required>                    
                     </div>
-                    <button type="submit" class="btn btn-primary w-100">Submit</button>
+                    <button type="submit" class="btn btn-primary w-75">Connexion</button>
                 </fieldset>
             </form>
             <fieldset>
-                <p>Vous n'avez pas de compte? <a href="signUp.php">Inscrivez-vous</a></p>
+                <p class="mt-3">Vous n'avez pas de compte? <a href="signUp.php">Inscrivez-vous</a></p>
             </fieldset> 
         </div>   
     </main>
     
 </body>
 </html>
+
