7c3c9a1cbc90712f9f821c27def81eaa5693e01f
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-04-30 12:08:50 +0200

    ajout index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Plateforme_ Network</title>
+	<meta name="description" content="Reseau Social pour La Plateforme_." />
+	<meta name="keywords" content="stories, posts, social network, followers, réseau social, La Plateforme_, plateformeurs" />
+	<meta name="author" content="Denis Farkas Emmanuel Cabassot Thuc-nhi Wiedenhofer" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-xl-6 col-sm-12">
+                <img src="assets/images/phone.png" alt="mobile">
+            </div>
+            <div class="col-xl-6 col-sm-12">
+            <form>
+                <fieldset>  
+                              
+
+                    <div class="form-group">               
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" required>               
+                    </div>
+                    <div class="form-group">             
+                        <input type="password" class="form-control" id="password" placeholder="Password" required>                    
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100">Submit</button>
+                </fieldset>
+            </form>
+            <fieldset>
+                <p>Vous n'avez pas de compte? <a href="signUp.php">Inscrivez-vous</a></p>
+            </fieldset> 
+        </div>   
+    </main>
+    
+</body>
+</html>
