1cf8a7b593fe63829ee97424637abb6826fcc287
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-05-06 17:02:43 +0200

    ajout model friend.php

---
+++ api/controllers/listFriends.php
@@ -0,0 +1,56 @@
+<?php
+
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+// get database connection
+include_once '../config/database.php';
+  
+// instantiate product object
+include_once '../models/Friend.php';
+  
+$database = new Database();
+$db = $database->getConnection();
+
+// instantiate user object
+$friend = new Friend($db);
+
+$stmt = $friend->listFriends($id_user);
+$num = $stmt->rowCount();
+
+if ($num>0){
+    $friend_arr=array();
+    $friend_arr['records']=array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        $friend_item = array(
+            "id_friend" => $id_friend,
+            "id_user" => $id_user,
+            "id_user_friend" => $id_user_friend            
+        );
+
+        array_push($friend_arr["records"], $friend_item);
+
+    }
+
+      // set response code - 200 OK
+    http_response_code(200);
+  
+    // show products data
+    echo json_encode($friend_arr);
+}
+  
+else{
+    // set response code - 404 Not found
+    http_response_code(404);  
+    
+}
+
---
+++ api/models/Friend.php
@@ -0,0 +1,93 @@
+<?php
+class Friend{
+
+    // database connection and table name
+    private $conn;
+    
+
+    // object properties
+    public $id_friend;    
+    public $id_user;
+    public $id_user_friend; 
+    public $confirmed;   
+    public $name;
+    public $lastname;
+    public $email;
+    public $avatar;
+
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    function listFriends($id_user){
+
+    // select all query
+    $query = 'SELECT id_friend, id_user_friend, users.name, users.lastname, email, avatar FROM friend JOIN users ON id_user_friend= users.id_users WHERE confirmed= "oui" LIMIT 20 OFFSET 20';
+
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+        
+    // execute query
+    $stmt->execute();
+
+    return $stmt;
+
+    }
+
+    function invitFriend($id_user, $id_user_friend){
+        // query to insert record
+        $query = "INSERT INTO friend SET id_user=:id_user, id_user_friend=:id_user_friend, confirmed= 'non'";
+    
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+       
+        $id_user=htmlspecialchars($id_user);
+        $id_user_friend=htmlspecialchars($id_user_friend);
+                        
+        // bind values
+      
+        $stmt->bindParam(":id_user", $id_user);
+        $stmt->bindParam(":id_user_group", $id_user_friend);
+                
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+            return false;    
+    }
+
+    function confirmFriend($id_friend){
+        // query to update record
+        $query = "UPDATE friend SET confirmed='oui' WHERE id_friend=:id_friend";
+            
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+        $id_friend=htmlspecialchars($id_friend);
+                        
+        // bind values
+      
+        $stmt->bindParam(":id_friend", $id_friend);
+
+        if($stmt->execute()){
+            return true;
+        }
+            return false; 
+
+    }
+        
+    function delete_friend($id_friend){        
+        $del= " DELETE FROM friend WHERE id_friend = :id_friend";
+        $stmt = $this->conn->prepare($del);
+
+        $stmt->bindParam(':id_friend', $id_friend);            
+       
+        if($stmt->execute()){
+            return true;
+        }               
+            return false;            
+    }
+
+}
