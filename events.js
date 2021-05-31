7561ad254501cfdf66c239a275d7c0e6673a93f4
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-05-31 14:10:00 +0200

    ajout event

--- events.js
+++ events.js
@@ -74,6 +74,37 @@ function viewEvent(id_event){
         xhr.send();
 }
 
+
+function suggestEventList(){
+		
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4 && this.status === 200) {
+            
+            var response = JSON.parse(xhr.responseText);
+            var records = response.records;
+            var output = '';
+
+            for(var i =0; i<records.length; i++){				
+                
+                    output +=  
+                    '<a href="#" class="list-group-item list-group-item-action"><img src="assets/images/upload/events/'+records[i].img_event+'" class="card-img-thumb" alt="...">'+records[i].title_event+
+                    ' le: '+records[i].date_event+' - '+records[i].city_event+')</a>';
+            }
+  
+            suggestedEvent.innerHTML = output;
+
+        }else  if(this.readyState === 4 && this.status === 404){
+            suggestedEvent.innerHTML = '<p class="list-group-item list-group-item-action">Pas de suggestion d\'évènements</p>';
+           
+            }  
+    });
+   
+    xhr.open("POST", 'api/controllers/suggestEvent');
+    xhr.send();
+}
+
+
  function listEvent(){
 		
     var xhr = new XMLHttpRequest();
