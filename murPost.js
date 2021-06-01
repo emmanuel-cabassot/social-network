aca9c6c0e708003e9811680e17684862002b3044
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2021-06-01 15:12:33 +0200

    enleve max width

--- murPost.js
+++ murPost.js
@@ -15,24 +15,27 @@ xhr.onreadystatechange = function () {
         let idPost = JSON.stringify(xhr.response)
         listIdPost = JSON.parse(idPost)
 
-        listIdPost.forEach(id_post => {
-            data = {
-                id_post: id_post.id_post
-            }
-
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", "api/controllers/postShow.php");
-            xhr.setRequestHeader("Content-Type", "text/plain");
-            xhr.responseType = "json";
-            xhr.send(JSON.stringify(data));
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4 || xhr.status == 201) {
-                    let post = JSON.stringify(xhr.response)
-                    posts = JSON.parse(post)
-                    console.log(posts)
+        if (!empty(listIdPost)) {
+            listIdPost.forEach(id_post => {
+                data = {
+                    id_post: id_post.id_post
                 }
-            }
-        });
+    
+                let xhr = new XMLHttpRequest();
+                xhr.open("POST", "api/controllers/postShow.php");
+                xhr.setRequestHeader("Content-Type", "text/plain");
+                xhr.responseType = "json";
+                xhr.send(JSON.stringify(data));
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4 || xhr.status == 201) {
+                        let post = JSON.stringify(xhr.response)
+                        posts = JSON.parse(post)
+                        console.log(posts)
+                    }
+                }
+            });
+        }
+        
     }
 }
 
